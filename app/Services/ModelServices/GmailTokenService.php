f3587045c940ff509cd7f2914a23592f306ecf46
Nate <nate.ha.goldenowl@gmail.com>
2023-10-31 09:05:29 +0700

    CH-14 setup connection

---
+++ app/Services/ModelServices/GmailTokenService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Services\ModelServices;
+use App\Models\GmailToken;
+use App\Models\User;
+use Google_Client;
+use Google_Service_Gmail;
+use Google_Service_PeopleService;
+
+
+class GmailTokenService extends BaseService
+{
+    private function getAccessToken($user) {
+        if(is_numeric($user)) {
+            $user = User::where('id', $user)->first();
+        }
+
+        return $user->gmailToken->access_token;
+    }
+
+    public function getEmailFromToken($token)
+    {
+        $client = new Google_Client();
+        $client->setAccessToken($token);
+        $service = new Google_Service_PeopleService($client);
+
+        $person = $service->people->get('people/me', ['personFields' => 'emailAddresses']);
+        return $person->getEmailAddresses()[0]->getValue();
+    }
+
+    public function create($data) {
+        return GmailToken::create($data);
+    }
+
+    public function getGmailService($user) {
+        $client = new Google_Client();
+        $client->setAccessToken($this->getAccessToken($user));
+        $service = new Google_Service_Gmail($client);
+    
+        return $service;
+    }
+}
--- app/Services/ModelServices/UserService.php
+++ app/Services/ModelServices/UserService.php
@@ -2,11 +2,20 @@
 
 namespace App\Services\ModelServices;
 use App\Constants\UserConstant\UserRole;
+use App\Constants\UserConstant\UserStatus;
 use App\Models\User;
 
 class UserService extends BaseService {
     public function getAllOwner() {
         $owners = User::whereNot('role', UserRole::ADMIN)->get();
         return $owners;
+    }
+
+    public function isEmailExist($email) {
+        return User::where('email', $email)->where('status', UserStatus::ACTIVE)->exists();
+    }
+
+    public function create($data) {
+        return User::create($data);
     }
 }
